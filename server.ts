435b30020eed3b14b550bd49dbb84dddf61c7e6b
Ghost00327 <talent.webstar0225@gmail.com>
2022-08-22 22:50:52 +0300

    upgrade to typescript
    Co-authored-by: toptal126 <toptal126@gmail.com>

---
+++ server.ts
@@ -0,0 +1,58 @@
+import express from "express";
+// import morgan from "morgan"
+const morgan = require('morgan');
+const bodyParser = require("body-parser");
+import bearerToken from "express-bearer-token";
+import errorMiddleware from "./src/middlewares/error.middleware"
+import swaggerUi from "swagger-ui-express"
+const swaggerDocument = require('./swagger.json');
+
+const app = express();
+const port = 8080;
+
+const cors = require("cors");
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+} else {
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+}
+
+app.use(bearerToken());
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+import db from "./src/db";
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+app.use(morgan('tiny'));
+app.get("/", async (req, res) => {
+  res.send("Hello there");
+});
+require("./src/routes/index.routes.ts")(app);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(errorMiddleware);
+
+app.listen(port, () => {
+  console.log(`Tasks server listening at http://localhost:${port}`);
+});
